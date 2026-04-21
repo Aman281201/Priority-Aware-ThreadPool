ed97ebd19e9fbf91e26a7cce16bd88ad1e192b9d
Aman Kumar <brofistaman@gmail.com>
2026-04-21 22:19:52 +0530

    updated the queue to a priority and made class for task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <chrono>
 #include "threadpool.h"
 
 // task function 1
@@ -9,6 +10,7 @@ void add_task_func(vector<int> input) {
     for(int n: input)
     sum += n;
 
+    this_thread::sleep_for(chrono::milliseconds(10));
     cout << "Sum: " << sum << endl;
 }
 
@@ -17,22 +19,26 @@ void multiply_task_func(vector<int> input) {
     int prod = 1;
     for(int n : input) prod *= n;
 
+    this_thread::sleep_for(chrono::milliseconds(10));
     cout << "Product: " << prod << endl;
 }
 
 int main() {
     ThreadPool pool(3);
 
-    // inputs
-    vector<int> v1 = {1, 2, 3, 4, 5};
-    vector<int> v2 = {10, 20, 30};
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2 = {4, 5, 6};
 
-    pool.add_task(add_task_func, v1);
-    pool.add_task(multiply_task_func, v1);
-    pool.add_task(add_task_func, v2);
-    pool.add_task(multiply_task_func, v2);
+    pool.add_task("T#1", 10, add_task_func, v1);
+    pool.add_task("T#2", 9, multiply_task_func, v1);
+    pool.add_task("T#3", 8, add_task_func, v2);
+    pool.add_task("T#4", 7, multiply_task_func, v2);
+    pool.add_task("T#5", 6, add_task_func, v1);
+    pool.add_task("T#6", 5, multiply_task_func, v1);
+    pool.add_task("T#7", 4, add_task_func, v2);
+    pool.add_task("T#8", 3, multiply_task_func, v2);
 
-    this_thread::sleep_for(chrono::milliseconds(100));
+    this_thread::sleep_for(chrono::milliseconds(200));
 
     return 0;
 }
--- threadpool.h
+++ threadpool.h
@@ -7,12 +7,28 @@
 #include <mutex>
 #include <condition_variable>
 #include <functional>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
+class Task {
+public:
+    string name;
+    int priority;
+    function<void()> func;
+
+    Task(string n, int p, function<void()> f) : name(n), priority(p), func(f) {}
+
+    // lower priority number means higher task priority.
+    bool operator<(const Task& other) const {
+        return priority > other.priority;
+    }
+};
+
 class ThreadPool {
 private: 
-    queue<function<void()>> task_queue;
+    priority_queue<Task> task_queue;
     vector<thread> threads;
     mutex queue_lock;
     condition_variable cv;
@@ -24,7 +40,7 @@ public:
         for(int i = 0; i < size; i++) {
             threads.push_back(thread([this](){
                 for(;;) {
-                    function<void()> task;
+                    function<void()> task_func;
                     {
                         unique_lock<mutex> lk(this->queue_lock);
                         cv.wait(lk, [this](){ 
@@ -33,20 +49,25 @@ public:
 
                         if(this->stop && this->task_queue.empty()) return;
                         
-                        task = move(this->task_queue.front());
+                        task_func = move(this->task_queue.top().func);
                         this->task_queue.pop();
                     }
-                    task();
+                    task_func();
                 }
             }));
         }
     }
 
-    void add_task(function<void(vector<int>)> task_func, vector<int> args) {
+    void add_task(string name, int priority, function<void(vector<int>)> task_func, vector<int> args) {
         {
             unique_lock<mutex> lk(this->queue_lock);
             if(stop) return;
-            task_queue.push([task_func, args](){ task_func(args); });
+            
+            cout << "Enqueuing task: " << name << " (Priority: " << priority << ")" << endl;
+            
+            task_queue.push(Task(name, priority, [task_func, args](){ 
+                task_func(args); 
+            }));
         }
         cv.notify_one();
     }
