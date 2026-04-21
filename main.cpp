e49b32c93ddcfed28d2fe3c5c602a8e0204bcf1f
Aman Kumar <brofistaman@gmail.com>
2026-04-21 21:46:11 +0530

    added testing driver code

--- main.cpp
+++ main.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
-#include <string>
-#include <thread>
 #include <vector>
-#include <algorithm>
-#include <unistd.h>
+#include <numeric>
+#include "threadpool.h"
 
-using namespace std;
+// task function 1
+void add_task_func(vector<int> input) {
+    int sum = 0;
+    for(int n: input)
+    sum += n;
 
-void function()
-{
-    for(int i = 0; i < 1000; i++)
-    cout<<"|";
+    cout << "Sum: " << sum << endl;
 }
 
-void function2()
-{
-    for(int i = 0; i < 1000; i++)
-    cout<<"-";
-}
+// task function 2
+void multiply_task_func(vector<int> input) {
+    int prod = 1;
+    for(int n : input) prod *= n;
 
+    cout << "Product: " << prod << endl;
+}
 
 int main() {
-    cout<<"start"<<endl;
-    thread w1(function);
-    thread w2(function2);
-    cout<<"aman";
-    w2.join();
+    ThreadPool pool(3);
 
-    w1.join();
+    // inputs
+    vector<int> v1 = {1, 2, 3, 4, 5};
+    vector<int> v2 = {10, 20, 30};
 
-    
+    pool.add_task(add_task_func, v1);
+    pool.add_task(multiply_task_func, v1);
+    pool.add_task(add_task_func, v2);
+    pool.add_task(multiply_task_func, v2);
+
+    this_thread::sleep_for(chrono::milliseconds(100));
+
     return 0;
 }
